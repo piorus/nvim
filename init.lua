f14368ed77d7aedc5a566e4f7fd6299f19f3d802
Piotr Rusin <piotr@rusin.work>
2024-07-13 11:41:16 +0000

    lsp

--- init.lua
+++ init.lua
@@ -1,5 +1,22 @@
 require("config.lazy")
 
+local lsp_zero = require('lsp-zero')
+lsp_zero.extend_lspconfig()
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  },
+})
+
 local o = vim.o
 
 o.expandtab = true 
---
+++ lua/plugins/lsp-zero.lua
@@ -0,0 +1,14 @@
+--- https://lsp-zero.netlify.app/v3.x/tutorial.html
+return {
+  'VonHeikemen/lsp-zero.nvim',
+  dependencies = {
+    'folke/tokyonight.nvim',
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/nvim-cmp',
+    'L3MON4D3/LuaSnip',
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim'
+  },
+  opts = {}
+}
