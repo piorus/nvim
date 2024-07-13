4b002f7bbfd725bc7d3e264ff8f34e3c42e7bb09
Piotr Rusin <piotr@rusin.work>
2024-07-13 17:08:29 +0000

    move config into plugins

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -11,5 +11,82 @@ return {
     'williamboman/mason-lspconfig.nvim',
     'onsails/lspkind-nvim'
   },
-  opts = {}
+  config = function()
+    local lsp_zero = require('lsp-zero')
+    lsp_zero.extend_lspconfig()
+    lsp_zero.on_attach(function(client, bufnr)
+      -- see :help lsp-zero-keybindings
+      -- to learn the available actions
+      lsp_zero.default_keymaps({buffer = bufnr})
+    end)
+
+    require('mason').setup({})
+    require('mason-lspconfig').setup({
+      handlers = {
+        function(server_name)
+          require('lspconfig')[server_name].setup({})
+        end,
+      },
+    })
+
+    require('lspconfig').intelephense.setup({})
+
+    local luasnip = require("luasnip")
+    local types = require("luasnip.util.types")
+    luasnip.config.setup({
+      ext_opts = {
+          [types.choiceNode] = {
+              active = {virt_text = {{"⇥", "GruvboxRed"}}}
+          },
+          [types.insertNode] = {
+              active = {virt_text = {{"⇥", "GruvboxBlue"}}}
+          }
+      }
+    })
+    local cmp = require("cmp")
+    local lspkind = require("lspkind")
+    cmp.setup({
+        snippet = {
+            expand = function(args)
+                luasnip.lsp_expand(args.body)
+            end
+        },
+        window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered()
+        },
+        mapping = cmp.mapping.preset.insert({
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-e>"] = cmp.mapping.abort(),
+            ["<CR>"] = cmp.mapping.confirm({select = true}),
+            ["<Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                elseif luasnip.locally_jumpable(1) then
+                    luasnip.jump(1)
+                else
+                    fallback()
+                end
+            end, {"i", "s"})
+        }),
+        sources = cmp.config.sources({
+            {name = "nvim_lsp"}, {name = "luasnip"}, {name = "buffer"}
+        }),
+        formatting = {
+            format = lspkind.cmp_format({
+                mode = "symbol_text",
+                maxwidth = 70,
+                show_labelDetails = true
+            })
+        },
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        }
+      })
+  end,
+  opts = {},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,5 +2,29 @@
 return {
   'nvim-telescope/telescope.nvim', 
   tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-file-browser.nvim' }
+  dependencies = { 
+    'nvim-lua/plenary.nvim', 
+    'nvim-telescope/telescope-file-browser.nvim' 
+  },
+  config = function()
+    local telescope = require("telescope")
+    telescope.setup {
+      extensions = {
+        file_browser = {
+          theme = "ivy",
+          -- disables netrw and use telescope-file-browser in its place
+          hijack_netrw = true,
+          mappings = {
+            ["i"] = {
+              -- your custom insert mode mappings
+            },
+            ["n"] = {
+              -- your custom normal mode mappings
+            },
+          },
+        },
+      },
+    }
+    telescope.load_extension "file_browser"
+  end
 }
