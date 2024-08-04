2c545f830a8fc7389eeb34febfe6e83eebcf7e4a
piorus <piotr@rusin.work>
2024-08-04 14:16:09 +0200

    update

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,29 +1,27 @@
-local opts = {noremap = true, silent = true}
-
 vim.g.mapleader = "<Space>"
 
 --- Telescope
 local telescope = require("telescope.builtin")
 vim.keymap.set("n", "-", ":Telescope file_browser<CR>")
-vim.keymap.set("n", "<leader>ff", telescope.find_files, opts) -- Lists files in your current working directory, respects .gitignore
-vim.keymap.set("n", "<leader>fx", telescope.treesitter, opts) -- Lists tree-sitter symbols
-vim.keymap.set("n", "<leader>fs", telescope.spell_suggest, opts) -- Lists spell options
+vim.keymap.set("n", "<leader>ff", telescope.find_files) -- Lists files in your current working directory, respects .gitignore
+vim.keymap.set("n", "<leader>fx", telescope.treesitter) -- Lists tree-sitter symbols
+vim.keymap.set("n", "<leader>fs", telescope.spell_suggest) -- Lists spell options
 
 --- Barbar
-vim.api.nvim_set_keymap("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
-vim.api.nvim_set_keymap("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
-vim.api.nvim_set_keymap("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
-vim.api.nvim_set_keymap("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>1", "<Cmd>BufferGoto 1<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>2", "<Cmd>BufferGoto 2<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>3", "<Cmd>BufferGoto 3<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>4", "<Cmd>BufferGoto 4<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>5", "<Cmd>BufferGoto 5<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>6", "<Cmd>BufferGoto 6<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>7", "<Cmd>BufferGoto 7<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>8", "<Cmd>BufferGoto 8<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>9", "<Cmd>BufferGoto 9<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>0", "<Cmd>BufferLast<CR>", opts)
-vim.api.nvim_set_keymap("n", "<A-p>", "<Cmd>BufferPin<CR>", opts)
-vim.api.nvim_set_keymap("n", "<A-q>", "<Cmd>BufferClose<CR>", opts)
-vim.api.nvim_set_keymap("n", "<A-w>", "<Cmd>BufferCloseAllButCurrent<CR>", opts)
+vim.keymap.set("n", "<A-,>", "<cmd>BufferPrevious<CR>")
+vim.keymap.set("n", "<A-.>", "<cmd>BufferNext<CR>")
+vim.keymap.set("n", "<A-<>", "<cmd>BufferMovePrevious<CR>")
+vim.keymap.set("n", "<A->>", "<cmd>BufferMoveNext<CR>")
+vim.keymap.set("n", "<A-1>", "<cmd>BufferGoto 1<CR>")
+vim.keymap.set("n", "<A-2>", "<cmd>BufferGoto 2<CR>")
+vim.keymap.set("n", "<A-3>", "<cmd>BufferGoto 3<CR>")
+vim.keymap.set("n", "<A-4>", "<cmd>BufferGoto 4<CR>")
+vim.keymap.set("n", "<A-5>", "<cmd>BufferGoto 5<CR>")
+vim.keymap.set("n", "<A-6>", "<cmd>BufferGoto 6<CR>")
+vim.keymap.set("n", "<A-7>", "<cmd>BufferGoto 7<CR>")
+vim.keymap.set("n", "<A-8>", "<cmd>BufferGoto 8<CR>")
+vim.keymap.set("n", "<A-9>", "<cmd>BufferGoto 9<CR>")
+vim.keymap.set("n", "<A-0>", "<cmd>BufferLast<CR>")
+vim.keymap.set("n", "<A-p>", "<cmd>BufferPin<CR>")
+vim.keymap.set("n", "<A-q>", "<cmd>BufferClose<CR>")
+vim.keymap.set("n", "<A-w>", "<cmd>BufferCloseAllButCurrent<CR>")
